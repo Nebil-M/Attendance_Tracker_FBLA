0e90230c48cca0b3b4811a20c666a7989038c37b
Nebil <hiachi1118@gmail.com>
2022-11-06 16:32:11 -0500

    Created Main

---
+++ main.py
@@ -0,0 +1,20 @@
+from kivy.app import App
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.widget import Widget
+
+
+class RootW(BoxLayout):
+    pass
+
+
+class AttendanceApp(App):
+    pass
+
+
+if __name__ == "__main__":
+    AttendApp = AttendanceApp()
+    AttendApp.run()
