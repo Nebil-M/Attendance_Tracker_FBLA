837850d365a6a31c7f4d36b42b2bcb544bd99ab6
Nebil <hiachi1118@gmail.com>
2022-11-06 23:00:22 -0500

    Added demos

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.screenmanager import Screen
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
 
 
-class RootW(BoxLayout):
+class RootW(Screen):
     pass
 
 
