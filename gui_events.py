bdf7ecffa696d8257a52598ca1a9aa479b54f2c6
Nebil <hiachi1118@gmail.com>
2022-12-17 22:55:26 -0500

    Edited the function in func_utils to remove bug sources. Updated the add, edit, and view frames for events Gui. Updated the style of Datable to correspond with dark mode. The view of Events greatly changed. Cleaned up code in main for nav bar, included an alt nav bar view as a class(with the nav bar horizontally placed instead of vertically) to facilitate comparison between them.

--- gui_events.py
+++ gui_events.py
@@ -9,41 +9,63 @@ from Events import EventManager
 class EventsFrame(ct.CTkFrame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.configure(fg_color="transparent")
 
         table = EventsTable(self)
         em = EventManager()
         table.load_events(em.events)
-        table.grid(row=0, column=0, sticky='NSEW', padx=30, pady=30)
+        table.grid(row=0, column=0, sticky='NSEW', padx=10, pady=30)
 
-        EventTools(self).grid(row=0, column=1, sticky='NSEW')
+        EventTools(self).grid(row=0, column=1, sticky='NSEW', padx=10, pady=30)
 
         self.rowconfigure(0, weight=1)
-        self.columnconfigure(0, weight=2)
+        self.columnconfigure(0, weight=10)
         self.columnconfigure(1, weight=1)
 
 
 class EventsTable(ct.CTkFrame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Initializing and setting size
-        self.tree = ttk.Treeview(self)
-        self.scroll = ttk.Scrollbar(self, orient="vertical", command=self.tree.yview)
-        self.tree.configure(yscroll=self.scroll.set)
+        # Setting Style
+        s = ttk.Style()
+        s.theme_use('clam')
+        s.configure('Treeview', fieldbackground='#343638', rowheight=40)
+        s.configure('Treeview.Heading',
+                    background="#343638", foreground='gray',
+                    font=('Helvetica', 20, 'bold'), fieldbackground='#343638')
 
-        # Gridding in the table and scroller
+        # Initializing treeview, and scroller
+        self.tree = ttk.Treeview(self)
+        self.scroll_y = ct.CTkScrollbar(self, orientation="vertical", command=self.tree.yview,
+                                        button_color=("gray55", "gray41"),
+                                        button_hover_color=(
+                                            "gray40", "gray53"))  # Must specify colors because of ct bug
+        self.tree.configure(yscroll=self.scroll_y.set)
+
+        self.scroll_x = ct.CTkScrollbar(self, orientation="horizontal", command=self.tree.xview,
+                                        button_color=("gray55", "gray41"),
+                                        button_hover_color=(
+                                            "gray40", "gray53"))  # Must specify colors because of ct bug
+        self.tree.configure(xscroll=self.scroll_x.set)
+
+        # Griding in the table and scroller
         self.tree.grid(row=0, column=0, sticky='NSEW')
-        self.scroll.grid(row=0, column=1, sticky='NSEW')
-        # self.rowconfigure(0, weight=1)
+        self.scroll_y.grid(row=0, column=1, sticky='NSEW')
+        self.scroll_x.grid(row=1, column=0)
+        self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
 
         # Setting up columns
         self.tree.heading("#0", text='ID', )
-        self.tree.column("#0", width=100, anchor='center')
+        self.tree.column("#0", width=200, anchor='center', minwidth=200)
         columns = ("Event Name", 'Date', 'Nature', "Description")
         self.tree["columns"] = columns
         for column in columns:
-            self.tree.heading(column, text=column, )
-            self.tree.column(column, width=100, anchor='center')
+            self.tree.heading(column, text=column)
+            if column == 'Event Name':
+                self.tree.column(column, width=300, anchor='center', minwidth=300)
+            else:
+                self.tree.column(column, width=200, anchor='center', minwidth=200)
 
     def add_event(self, event):
         values = (event.name, event.date, event.is_sport, event.event_description)
@@ -55,12 +77,16 @@ class EventsTable(ct.CTkFrame):
             values = (event.name, event.date, event.nature, event.event_description)
             event_id = str(event.event_id)
             self.tree.insert("", 'end', event_id, text=event_id, values=values, tags=('ttk', 'simple'))
-            self.tree.tag_configure('ttk', background='lightGray', font=('Helvetica', 12))
+            self.tree.tag_configure('ttk', font=('Helvetica', 20, 'bold'), foreground='gray74', background='#343638')
 
 
 class EventTools(ct.CTkFrame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # Grid layout:
+        self.columnconfigure(0, weight=1)
+        # self.rowconfigure(0, weight=1)
+
         # initializing frames to an empty array
         self.frames = {}
 
@@ -78,34 +104,44 @@ class EventTools(ct.CTkFrame):
         # Nav frame
         self.nav_frame = ct.CTkFrame(self)
         self.nav_frame.grid(row=0, column=0, sticky='NWS')
-        self.columnconfigure(0, weight=1)
-        # self.rowconfigure(0, weight=1)
+        self.nav_frame.configure(fg_color="transparent")
+
+        # Nav_frame layout:
+        self.nav_frame.columnconfigure(0, weight=1)
+        self.nav_frame.columnconfigure(1, weight=1)
+        self.nav_frame.columnconfigure(2, weight=1)
 
         # Nav buttons
+        font = ('arial', 16, 'bold')
+        border_width = 2
+        border_color = ("#3B8ED0", "#1F6AA5")
+        corner_radius = 5
+        fg_color = ("#3B8ED0", "#1F6AA5")
+        text_color = ("#DCE4EE", "#DCE4EE")
+        hover_color = ('#325882', '#14375e')
+        height = 40
+        anchor = "center"
+
         nav_add = ct.CTkButton(self.nav_frame, text="Add tab", command=lambda: self.show_frame(AddTab, nav_add),
-                               border_width=4, border_color=("#3B8ED0", "#1F6AA5"), corner_radius=20,
-                               fg_color=("#3B8ED0", "#1F6AA5"), text_color=("#DCE4EE", "#DCE4EE"),
-                               hover_color=("#3E454A", "#949A9F"), height=40,
-                               anchor="w", )
+                               border_width=border_width, border_color=border_color, corner_radius=corner_radius,
+                               fg_color=fg_color, text_color=text_color, font=font,
+                               hover_color=hover_color, height=height,
+                               anchor=anchor, )
         nav_add.grid(row=0, column=0, sticky='NEWS')
-        self.nav_frame.columnconfigure(0, weight=1)
 
-        nav_del = ct.CTkButton(self.nav_frame, text="del tab", command=lambda: self.show_frame(EditTab, nav_del),
-                               border_width=4, border_color=("#3B8ED0", "#1F6AA5"), corner_radius=20,
-                               fg_color=("#3B8ED0", "#1F6AA5"), text_color=("#DCE4EE", "#DCE4EE"),
-                               hover_color=("#3E454A", "#949A9F"), height=40,
-                               anchor="w", )
+        nav_del = ct.CTkButton(self.nav_frame, text="Edit tab", command=lambda: self.show_frame(EditTab, nav_del),
+                               border_width=border_width, border_color=border_color, corner_radius=corner_radius,
+                               fg_color=fg_color, text_color=text_color, font=font,
+                               hover_color=hover_color, height=height,
+                               anchor=anchor, )
         nav_del.grid(row=0, column=1, sticky='NEWS')
-        self.nav_frame.columnconfigure(1, weight=1)
-        nav_view = ct.CTkButton(self.nav_frame, text="del tab", command=lambda: self.show_frame(ViewTab, nav_view),
-                               border_width=4, border_color=("#3B8ED0", "#1F6AA5"), corner_radius=20,
-                               fg_color=("#3B8ED0", "#1F6AA5"), text_color=("#DCE4EE", "#DCE4EE"),
-                               hover_color=("#3E454A", "#949A9F"), height=40,
-                               anchor="w", )
 
+        nav_view = ct.CTkButton(self.nav_frame, text="View tab", command=lambda: self.show_frame(ViewTab, nav_view),
+                                border_width=border_width, border_color=border_color, corner_radius=corner_radius,
+                                fg_color=fg_color, text_color=text_color, font=font,
+                                hover_color=hover_color, height=height,
+                                anchor=anchor, )
         nav_view.grid(row=0, column=2, sticky='NEWS')
-        self.nav_frame.columnconfigure(1, weight=1)
-
 
         self.show_frame(AddTab, nav_add)
 
@@ -113,7 +149,7 @@ class EventTools(ct.CTkFrame):
         if button:
             for child in self.nav_frame.winfo_children():
                 child.configure(fg_color=("#3B8ED0", "#1F6AA5"))
-            button.configure(fg_color=("#3E454A", "#949A9F"))
+            button.configure(fg_color=('#F9F9FA', '#343638'))
         frame = self.frames[cont]
         frame.tkraise()
 
@@ -121,7 +157,7 @@ class EventTools(ct.CTkFrame):
 class AddTab(ct.CTkFrame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # ct.CTkButton(self, text='ADD').grid(column=0, row=0, sticky='NSEW', padx=20, pady=20)
+        self.configure(fg_color="transparent")
         # Creating widgets
         font = 20
         self.name = ct.CTkEntry(self, placeholder_text='Event Name', font=('arial', font))
@@ -136,21 +172,21 @@ class AddTab(ct.CTkFrame):
         self.description.insert('0.0', 'Description of the event')
         # griding in entries
         pad = 10
-        self.name.grid(row=0, column=0, sticky='NEWS', padx=pad, pady=pad + 30)
-        self.date.grid(row=0, column=1, sticky='NEWS', padx=pad, pady=pad + 30)
+        self.name.grid(row=0, column=0, sticky='NEWS', padx=pad, pady=(pad + 30, pad))
+        self.date.grid(row=0, column=1, sticky='NEWS', padx=pad, pady=(pad + 30, pad))
         self.id.grid(row=1, column=0, sticky='NEWS', padx=pad, pady=pad + 10)
         self.nature.grid(row=1, column=1, sticky='NEWS', padx=pad, pady=pad + 10)
         self.description.grid(row=2, sticky='NEWS', padx=pad, pady=pad, columnspan=2)
         self.add.grid(row=3, sticky='NEWS', padx=pad, pady=pad, columnspan=2)
         self.delete.grid(row=4, sticky='NEWS', padx=pad, pady=pad, columnspan=2)
-        #
-
+        # applying a weight of 1 to all cells
         weight_cells_1(self)
 
 
 class EditTab(ct.CTkFrame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.configure(fg_color="transparent")
         # Creating widgets
         font = 20
         self.name = ct.CTkEntry(self, placeholder_text='Event Name', font=('arial', font))
@@ -165,49 +201,61 @@ class EditTab(ct.CTkFrame):
 
         # griding in entries
         pad = 10
-        self.name.grid(row=0, column=0, sticky='NEWS', padx=pad, pady=pad + 30)
-        self.date.grid(row=0, column=1, sticky='NEWS', padx=pad, pady=pad + 30)
+        self.name.grid(row=0, column=0, sticky='NEWS', padx=pad, pady=(pad + 30, pad))
+        self.date.grid(row=0, column=1, sticky='NEWS', padx=pad, pady=(pad + 30, pad))
         self.id.grid(row=1, column=0, sticky='NEWS', padx=pad, pady=pad + 10)
         self.nature.grid(row=1, column=1, sticky='NEWS', padx=pad, pady=pad + 10)
         self.description.grid(row=2, sticky='NEWS', padx=pad, pady=pad, columnspan=2)
-        self.edit_button.grid(row=3, sticky='NEWS', padx=pad+40, pady=pad, columnspan=2)
+        self.edit_button.grid(row=3, sticky='NEWS', padx=pad + 40, pady=pad, columnspan=2)
         # applying a weight of 1 to all cells
         weight_cells_1(self)
+
+        # Evening out the different tab alignment
+        self.rowconfigure(4, weight=1)
+        self.rowconfigure(5, weight=1)
 
 
 class ViewTab(ct.CTkFrame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.id = ct.CTkEntry(self, placeholder_text="Event ID", font=('arial', 20), height=5)
-        self.id_enter = ct.CTkButton(self, text="Confirm", font=('arial', 40), height=5)
+        self.configure(fg_color="transparent")
+        self.id = ct.CTkEntry(self, placeholder_text=" Event ID", font=('arial', 20), height=40)
+        self.id_enter = ct.CTkButton(self, text=" Select", font=('arial', 20), height=40, anchor='center',
+                                     corner_radius=6)
         # Student_list
         self.student_list = ct.CTkFrame(self, border_width=4, border_color='black', height=100)
         self.description = ct.CTkTextbox(self, height=200, font=('arial', 20))
+        # add_default text for description box
+        self.description.insert('0.0', 'Description of the event')
 
-        # gridding
+        # griding
         pad = 10
-        self.id.grid(row=0, column=0, sticky='NSEW', padx=(pad+90, pad+10), pady=(pad+40, pad+10))
-        self.id_enter.grid(row=0, column=1, sticky='NSEW', padx=(pad+10, pad+90), pady=(pad+40, pad+10))
+        self.id.grid(row=0, column=0, sticky='NSEW', padx=(pad + 90, pad + 10), pady=(pad + 40, pad + 10))
+        self.id_enter.grid(row=0, column=1, sticky='NSEW', padx=(pad + 10, pad + 90), pady=(pad + 40, pad + 10))
 
-        self.student_list.grid(row=1, column=0, sticky='NSEW', padx=pad+40, pady=pad, columnspan=2)
-        self.description.grid(row=2, column=0, sticky='NSEW', padx=pad+40, pady=pad, columnspan=2)
+        self.student_list.grid(row=1, column=0, sticky='NSEW', padx=pad + 40, pady=pad, columnspan=2)
+        self.description.grid(row=2, column=0, sticky='NSEW', padx=pad + 40, pady=pad, columnspan=2)
 
         # applying a weight of 1 to all cells
         weight_cells_1(self)
+
+        # Manually editing layout
+        self.rowconfigure(1, weight=3)
+        self.rowconfigure(2, weight=7)
 
 
 if __name__ == "__main__":
     window = ct.CTk()
     # Setting size of the window
-    window.geometry("1300x550")
+    # window.geometry("1300x550")
     ct.set_appearance_mode("dark")  # Modes: system (default), light, dark
     ct.set_default_color_theme("dark-blue")
 
-    #EventsFrame(window).grid(row=0, column=0, sticky='NSEW')
+    EventsFrame(window).grid(row=0, column=0, sticky='NSEW')
     # EventTools(window).grid(row=0, column=0, sticky='NSEW')
     # EventsTable(window).grid(row=0, column=0, sticky='NSEW')
 
-    ViewTab(window).grid(row=0, column=0, sticky='NSEW')
+    # ViewTab(window).grid(row=0, column=0, sticky='NSEW')
 
     # Making the widgets resizable
     window.rowconfigure(0, weight=1)
--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import customtkinter
 import customtkinter as ct
-from gui_events import*
+from gui_events import *
+from gui_students import *
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-        self.geometry("1000x600")
+        self.geometry("1400x600")
         self.title("Main")
         customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
         customtkinter.set_default_color_theme("dark-blue")
-        NavigationFrame(self).grid(padx=0, pady=0, row=0, column=0)
+        NavigationFrame(self).grid(padx=0, pady=0, row=0, column=0, sticky="NEWS")
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
 
 
 # For example Purposes
@@ -39,18 +42,120 @@ class Login(ct.CTkFrame):
             main_frame.columnconfigure(child.grid_info()['column'], weight=1, minsize=500)
 
 
-class NavigationFrame(customtkinter.CTkFrame):
-    def __init__(self, root):
-        super().__init__(root)
+# Nav bar at the top, Alternate view. Access by changing 'NavigationFrame' to 'NavigationFrameTop' in the App class.
+class NavigationFrameTop(customtkinter.CTkFrame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.configure(fg_color="transparent")
         # set grid layout
-        root.grid_rowconfigure(0, weight=1)
-        root.grid_columnconfigure(0, weight=1)
-        root.grid_columnconfigure(1, weight=15)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=15)
+        self.grid_columnconfigure(0, weight=1)
+
 
         # create navigation frame
-        self.navigation_frame = customtkinter.CTkFrame(root, corner_radius=0)
-        self.navigation_frame.grid(row=0, column=0, sticky="nsew")
-        self.navigation_frame.grid_rowconfigure(4, weight=1)
+        self.navigation_frame = customtkinter.CTkFrame(self, corner_radius=0)
+        self.navigation_frame.grid(row=0, column=0, sticky="new")
+        self.navigation_frame.columnconfigure(4, weight=1)
+
+        self.navigation_frame_label = customtkinter.CTkLabel(self.navigation_frame, text="Navigation",
+                                                             compound="left",
+                                                             font=customtkinter.CTkFont(size=15, weight="bold"))
+        self.navigation_frame_label.grid(row=0, column=0, padx=20, pady=20)
+
+        # Buttons to navigate
+        self.home_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10,
+                                                   text="Home",
+                                                   fg_color="transparent", text_color=("gray10", "gray90"),
+                                                   hover_color=("gray70", "gray30"),
+                                                   anchor="w", command=self.home_button_event)
+        self.home_button.grid(row=0, column=1, sticky="ew")
+
+        self.frame_2_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40,
+                                                      border_spacing=10, text="Students",
+                                                      fg_color="transparent", text_color=("gray10", "gray90"),
+                                                      hover_color=("gray70", "gray30"),
+                                                      anchor="w", command=self.frame_2_button_event)
+        self.frame_2_button.grid(row=0, column=2, sticky="ew")
+
+        self.frame_3_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40,
+                                                      border_spacing=10, text="Events",
+                                                      fg_color="transparent", text_color=("gray10", "gray90"),
+                                                      hover_color=("gray70", "gray30"),
+                                                      anchor="w", command=self.frame_3_button_event)
+        self.frame_3_button.grid(row=0, column=3, sticky="ew")
+
+        for child in self.navigation_frame.winfo_children():
+            self.navigation_frame.rowconfigure(child.grid_info()['row'], weight=1)
+            #self.navigation_frame.columnconfigure(child.grid_info()['column'], weight=1)
+
+        # Appearance changer
+        self.appearance_mode_menu = customtkinter.CTkOptionMenu(self.navigation_frame,
+                                                                values=["Dark", "Light", "System"],
+                                                                command=self.change_appearance_mode_event)
+        self.appearance_mode_menu.grid(row=0, column=6, padx=20, pady=20, sticky="s")
+
+        # create home frame
+        self.home_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+
+        # create second frame
+        self.second_frame = StudentsFrame(self)
+
+        # create third frame
+        self.third_frame = EventsFrame(self)
+
+        # select default frame
+        self.select_frame_by_name("home")
+
+    def select_frame_by_name(self, name):
+        # set button color for selected button
+        self.home_button.configure(fg_color=("gray75", "gray25") if name == "home" else "transparent")
+        self.frame_2_button.configure(fg_color=("gray75", "gray25") if name == "frame_2" else "transparent")
+        self.frame_3_button.configure(fg_color=("gray75", "gray25") if name == "frame_3" else "transparent")
+
+        # show selected frame
+        if name == "home":
+            self.home_frame.grid(row=1, column=0, sticky="nsew")
+        else:
+            self.home_frame.grid_forget()
+        if name == "frame_2":
+            self.second_frame.grid(row=1, column=0, sticky="nsew")
+        else:
+            self.second_frame.grid_forget()
+        if name == "frame_3":
+            self.third_frame.grid(row=1, column=0, sticky="nsew")
+        else:
+            self.third_frame.grid_forget()
+
+    def home_button_event(self):
+        self.select_frame_by_name("home")
+
+    def frame_2_button_event(self):
+        self.select_frame_by_name("frame_2")
+
+    def frame_3_button_event(self):
+        self.select_frame_by_name("frame_3")
+
+    def change_appearance_mode_event(self, new_appearance_mode):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+
+
+# Nav bar at the top, Default view. Access by changing 'NavigationFrameTop' to 'NavigationFrame' in the App class.
+class NavigationFrame(customtkinter.CTkFrame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.configure(fg_color="transparent")
+        # set grid layout
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=15)
+
+
+
+        # create navigation frame
+        self.navigation_frame = customtkinter.CTkFrame(self, corner_radius=0)
+        self.navigation_frame.grid(row=0, column=0, sticky="nws")
+        self.navigation_frame.rowconfigure(4, weight=1)
 
         self.navigation_frame_label = customtkinter.CTkLabel(self.navigation_frame, text="Navigation",
                                                              compound="left",
@@ -66,21 +171,21 @@ class NavigationFrame(customtkinter.CTkFrame):
         self.home_button.grid(row=1, column=0, sticky="ew")
 
         self.frame_2_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40,
-                                                      border_spacing=10, text="Frame 2",
+                                                      border_spacing=10, text="Students",
                                                       fg_color="transparent", text_color=("gray10", "gray90"),
                                                       hover_color=("gray70", "gray30"),
                                                       anchor="w", command=self.frame_2_button_event)
         self.frame_2_button.grid(row=2, column=0, sticky="ew")
 
         self.frame_3_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40,
-                                                      border_spacing=10, text="Frame 3",
+                                                      border_spacing=10, text="Events",
                                                       fg_color="transparent", text_color=("gray10", "gray90"),
                                                       hover_color=("gray70", "gray30"),
                                                       anchor="w", command=self.frame_3_button_event)
         self.frame_3_button.grid(row=3, column=0, sticky="ew")
 
         for child in self.navigation_frame.winfo_children():
-            # self.navigation_frame.rowconfigure(child.grid_info()['row'], weight=1)
+            #self.navigation_frame.rowconfigure(child.grid_info()['row'], weight=1)
             self.navigation_frame.columnconfigure(child.grid_info()['column'], weight=1)
 
         # Appearance changer
@@ -90,13 +195,13 @@ class NavigationFrame(customtkinter.CTkFrame):
         self.appearance_mode_menu.grid(row=6, column=0, padx=20, pady=20, sticky="s")
 
         # create home frame
-        self.home_frame = Login(root)
+        self.home_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
 
         # create second frame
-        self.second_frame = EventsFrame(root)
+        self.second_frame = StudentsFrame(self)
 
         # create third frame
-        self.third_frame = customtkinter.CTkFrame(root, corner_radius=0, fg_color="transparent")
+        self.third_frame = EventsFrame(self)
 
         # select default frame
         self.select_frame_by_name("home")
